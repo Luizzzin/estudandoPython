11ca08993214408a945cec8ed246a2751dd68a45
Luizzzin <133661950+Luizzzin@users.noreply.github.com>
2025-04-23 09:47:19 -0300

    aula03-lacos-repeticao

---
+++ aula03a-motivacao.py
@@ -0,0 +1,60 @@
+# ===============tabuada=============
+# num = int(input("DIGITE UM NUMERO DE TABUADA \n"))
+# denominador = 1
+#
+# while denominador <= 10:
+#     resultado = num * denominador
+#     print(f"{num}*{denominador} = {resultado}")
+#     denominador += 1
+
+
+# --------------------------
+
+# quebrar o continuar o laço
+
+# cp = 0
+# while cp < 10:
+#     cp += 1
+#
+#     if cp == 3 or cp == 4:
+#         #       continua o loop onde ele esta inserido.
+#         continue
+#     if cp == 7:
+#         break
+#
+#     print(f"{cp} FDS")
+# -------------------------------------
+
+# ========contador=========
+#
+# n = int(input("digite um num: \n"))
+# conta = 1
+#
+# while conta <= n:
+#     print(conta)
+#     conta += 1
+# ----------------------
+#
+
+# =====verificador de notas============
+
+# num1 = int(input("Digite sua nota:\n"))
+#
+# while num1 < 0 or num1 > 10:
+#     print("REPITA!!!!! entre 0 ou 10")
+#     num1 = int(input("Digite sua nota:\n"))
+#
+# num2 = int(input("Digite sua 2° nota:\n"))
+#
+# while num2 < 0 or num2 > 10:
+#     print("REPITA!!!!! entre 0 ou 10")
+#     num2 = int(input("Digite sua nota:\n"))
+#
+# media = (num1 + num2)/2
+# print(f"sua media é {media}")
+
+# ----------------------
+
+
+
+
---
+++ aula03b-for.py
@@ -0,0 +1,9 @@
+# for i in range(0,4):
+#     for j in range(0, 10 , 5):
+#         print(i,j)
+
+# -----------------------
+# produto = int(input("quantos produtos voce quer?:\n"))
+#
+# for i in range(produto):
+#     print(f"produto{i + 1}")
