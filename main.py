45e5951f92d5fa95310abd093d13f766586eca7e
Luizzzin <133661950+Luizzzin@users.noreply.github.com>
2025-09-04 11:18:39 -0300

    finish
    API FINALIZADA

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CHAVE_API_KEY = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=CHAVE_API_KEY)
+MODELO_ESCOLHIDO = "gemini-1.5-flash"
+
+prompt_sistema = "Liste apenas os nomes dos produtos e ofereça uma breve descrição de cada um"
+
+llm = genai.GenerativeModel(
+    model_name=MODELO_ESCOLHIDO,
+    system_instruction=prompt_sistema
+
+)
+
+pergunta = "liste três produtos de moda sustentavel para ir ao shopping."
+resposta = llm.generate_content(pergunta)
+print(f'Resposta gerada:{resposta.text}')
